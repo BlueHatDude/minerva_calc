06a7868f4e53b93e274ba5eaa8303e12ab21c594
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-04-18 09:36:28 -0400

    implemented tokenization

--- src/minerva_calc/solver.py
+++ src/minerva_calc/solver.py
@@ -16,6 +16,27 @@ def constrain_num(n: int, nmax: int, nmin: int) -> int:
     return max(min(nmax, n), nmin)
 
 
+def str_to_num(s: str) -> int | float | None:
+    """converts string into appropriate number type (does not handle negatives)
+
+    Args:
+        s (str): input
+
+    Returns:
+        int | float: if string is valid num
+        None: if string is not valid num
+    """    
+  
+    if s.isdigit():
+        return int(s)
+    else:
+        try:
+            f = float(s)
+            return f
+        except ValueError:
+            return None
+
+
 class SolutionSet:
 
     def __init__(self) -> None:
@@ -38,22 +59,62 @@ class TokenType(Enum):
     OP_EXPONENT = 5
     LEFT_PAR = 6
     RIGHT_PAR = 7
+    
     SQRT = 8
     SIN = 9
     COS = 10
     TAN = 11
-    LOG = 12
-    LN = 13
+    LOG_10 = 12
+    LOG_E = 13
     
     INTEGER = 14
     DECIMAL = 15
     SYMBOL = 16
 
 
+def type_to_str(inp_type: TokenType) -> str:
+    match inp_type:
+        case TokenType.EMPTY:
+            return "EMPTY"
+        case TokenType.OP_PLUS:
+            return "OP_PLUS"
+        case TokenType.OP_MINUS:
+            return "OP_MINUS"
+        case TokenType.OP_MULTIPLY:
+            return "OP_MULTIPLY"
+        case TokenType.OP_DIVIDE:
+            return "OP_DIVIDE"
+        case TokenType.OP_EXPONENT:
+            return "OP_EXPONENT"
+        case TokenType.LEFT_PAR:
+            return "LEFT_PAR"
+        case TokenType.RIGHT_PAR:
+            return "RIGHT_PAR"
+        case TokenType.SQRT:
+            return "SQRT"
+        case TokenType.SIN:
+            return "SIN"
+        case TokenType.COS:
+            return "COS"
+        case TokenType.TAN:
+            return "TAN"
+        case TokenType.LOG_10:
+            return "LOG_10"
+        case TokenType.LOG_E:
+            return "LOG_E"
+        case TokenType.INTEGER:
+            return "INTEGER"
+        case TokenType.DECIMAL:
+            return "DECIMAL "
+        case TokenType.SYMBOL:
+            return "SYMBOL"
+    
+
 class Token:
     
-    def __init__(self, type: TokenType) -> None:
-        self.type = TokenType
+    def __init__(self, type: TokenType, value: int | float=None) -> None:
+        self.type = type
+        self.value = value
         
 
 class Solver:
@@ -69,7 +130,7 @@ class Solver:
             inp[index] = s.lower()
 
 
-    def combine_nums(self, inp: list[str]):
+    def combine_nums(self, inp: list[str]) -> list[str]:
         """ a function that takes a list of strs and combines all adjacent digits into one number
 
 
@@ -101,7 +162,7 @@ class Solver:
         return new_inp
 
 
-    def combine_letters(self, inp: list[str]) -> None:
+    def combine_letters(self, inp: list[str]) -> list[str]:
         new_inp = inp
 
         for index, char in enumerate(new_inp):
@@ -119,21 +180,63 @@ class Solver:
 
 
     def _tokenize(self) -> None:
-        # 2 + 4 -> INTEGER:2 OP_PLUS INTEGER:4
-        str_tokens: list[str] = [char for char in self.equ]
-        self.combine_nums(str_tokens)
+        # preparing input
+        str_tokens: list[str] = list(self.equ)
+        str_tokens = self.combine_nums(str_tokens)
+        str_tokens = self.combine_letters(str_tokens)
+
+        for value in str_tokens:
+            match value:
+                case "+":
+                    self.tokens.append( Token(TokenType.OP_PLUS) )
+                case "-":
+                    self.tokens.append( Token(TokenType.OP_MINUS) )
+                case "*":
+                    self.tokens.append( Token(TokenType.OP_MULTIPLY) )
+                case "/":
+                    self.tokens.append( Token(TokenType.OP_DIVIDE) )
+                case "^":
+                    self.tokens.append( Token(TokenType.OP_EXPONENT) )
+                case "(":
+                    self.tokens.append( Token(TokenType.LEFT_PAR) )
+                case ")":
+                    self.tokens.append( Token(TokenType.RIGHT_PAR) )
+                case "sqrt":
+                    self.tokens.append( TokenType(TokenType.SQRT) )
+                case "sin":
+                    self.tokens.append( Token(TokenType.SIN) )
+                case "cos":
+                    self.tokens.append( Token(TokenType.COS) )
+                case "tan":
+                    self.tokens.append( Token(TokenType.TAN) )
+                case "log":
+                    self.tokens.append( Token(TokenType.LOG_10) )
+                case "ln":
+                    self.tokens.append( Token(TokenType.LOG_E) )
+                case _:
+                    num_val = str_to_num(value)
+                    if num_val is not None:
+                        if isinstance(num_val, int):
+                            self.tokens.append( Token(TokenType.INTEGER, num_val) )
+                        elif isinstance(num_val, float):
+                            self.tokens.append( Token(TokenType.DECIMAL, num_val) )
 
 
     def _parse(self) -> None:
+        """ 
+        """        
         ...
 
 
-    def _run_tests(self) -> None:
-        str_tokens: list[str] = [char for char in self.equ]
-        str_tokens = self.combine_nums(str_tokens)
-        str_tokens = self.combine_letters(str_tokens)
-        self.lower_all(str_tokens)
+    def _print_tokens(self) -> None:
+        str_tokens = [f"Index: {index} | Type: {type_to_str(element.type)} | Value: {element.value}"\
+                        for index, element in enumerate(self.tokens)]
         print(str_tokens)
+
+
+    def _run_tests(self) -> None:
+        self._tokenize()
+        self._print_tokens()
 
 
     def evaluate(self) -> None:
